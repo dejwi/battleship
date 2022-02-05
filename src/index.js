f31b3a43037a32530f80f0ba4c311368cd7d3c4d
dejwi <misti.krasula@gmail.com>
2022-02-05 22:21:12 +0100

    base of gameboard

--- src/index.js
+++ src/index.js
@@ -1,23 +1,72 @@
 // import "./style.scss";
 
-const shipFactory = (len, posx, posy) => {
+const shipFactory = (len, posx, posy, isX) => {
+  const isHorizontal = isX;
   const length = len;
   const startPos = {
     x: posx,
     y: posy,
   };
-  const hitPos = [];
-  const isSunk = () => {
-    return hitPos.length === length;
-  };
-  const hit = (n) => {
-    if (hitPos.includes(n)) return false;
-    if (n > length || n < length) return false;
+  let hitCount = 0;
 
-    hitPos.push(n);
-    return true;
+  const hit = () => {
+    hitCount += 1;
   };
-  return { length, startPos, hitPos, isSunk, hit };
+
+  const isSunk = () => {
+    return hitCount === length;
+  };
+
+  return { length, startPos, hitCount, isSunk, isHorizontal, hit };
 };
 
-export { shipFactory };
+const gameboardFactory = () => {
+  const sunk = [];
+  const getSunk = () => {
+    return sunk;
+  };
+  const attacked = [];
+
+  const ships = [];
+  const shipPlace = (ship) => {
+    ships.push(ship);
+  };
+
+  const getShip = (x, y) => {
+    // looks like shit but it works
+    return ships.filter((e) => {
+      if (e.isHorizontal) {
+        return (
+          e.startPos.x + e.length > x && e.startPos.x <= x && e.startPos.y === y
+        );
+      }
+      return (
+        e.startPos.y + e.length > y && e.startPos.y <= y && e.startPos.x === x
+      );
+    })[0];
+  };
+
+  const reciveAttack = (px, py) => {
+    const ship = getShip(px, py);
+    if (ship) {
+      ship.hit();
+      const shipSunk = ship.isSunk();
+
+      if (shipSunk) {
+        sunk.push({
+          x: ship.startPos.x,
+          y: ship.startPos.y,
+          length: ship.length,
+          isHorizontal: ship.isHorizontal,
+        });
+      }
+
+      attacked.push({ x: px, y: py, isHit: true, isSunk: shipSunk });
+      return 'hit';
+    }
+    attacked.push({ x: px, y: py, isHit: false, isSunk: false });
+    return 'miss';
+  };
+  return { shipPlace, reciveAttack, getSunk };
+};
+export { shipFactory, gameboardFactory };
--- src/scripts/tests/game.test.js
+++ src/scripts/tests/game.test.js
@@ -1,9 +1,33 @@
-import { shipFactory } from "../../index";
-
-test("Test destroying ship", () => {
-  const shipmock = shipFactory(3, 0, 0);
-  shipmock.hit(1);
-  shipmock.hit(2);
-  shipmock.hit(3);
+import { shipFactory, gameboardFactory } from '../../index';
+// TODO: create real jest mocks
+test('Ship destroy', () => {
+  const shipmock = shipFactory(3, 0, 0, true);
+  shipmock.hit();
+  shipmock.hit();
+  shipmock.hit();
   expect(shipmock.isSunk()).toBe(true);
+});
+
+describe('gameboard', () => {
+  const gameboardmock = gameboardFactory();
+  gameboardmock.shipPlace(shipFactory(3, 0, 0, true));
+
+  test('Ship take hit', () => {
+    expect(gameboardmock.reciveAttack(0, 0)).toBe('hit');
+  });
+  test('Miss', () => {
+    expect(gameboardmock.reciveAttack(3, 0)).toBe('miss');
+  });
+  test('Destroy ship', () => {
+    gameboardmock.reciveAttack(1, 0);
+    gameboardmock.reciveAttack(2, 0);
+    expect(gameboardmock.getSunk()).toEqual([
+      {
+        x: 0,
+        y: 0,
+        length: 3,
+        isHorizontal: true,
+      },
+    ]);
+  });
 });
