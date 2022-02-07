75f2ebecd731222b66f07ef7036452ea444a3f9a
dejwi <misti.krasula@gmail.com>
2022-02-07 01:27:25 +0100

    ship placement system

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -18,8 +18,16 @@ function domMain() {
     };
   }
 
-  function addHover(depth, element, isFit, isHorizontal = true) {
-    const classAdd = isFit ? 'hoverfit' : 'hovernofit';
+  function addClasses(
+    depth,
+    element,
+    isFit,
+    isHorizontal = true,
+    place = false
+  ) {
+    let classAdd = isFit ? 'hoverfit' : 'hovernofit';
+    if (place) classAdd = 'pship';
+
     const startPos = {
       x: element.getAttribute(`data-x`),
       y: element.getAttribute(`data-y`),
@@ -27,34 +35,71 @@ function domMain() {
 
     for (let i = 0; i < depth; i++) {
       if (isHorizontal) {
-        document.querySelector(
+        const e = document.querySelector(
           `.player div[data-x="${+startPos.x + i}"][data-y="${startPos.y}"]`
-        ).classList = classAdd;
+        );
+        if (!e.classList.value.includes('pship')) e.classList = classAdd;
       } else {
-        document.querySelector(
+        const e = document.querySelector(
           `.player div[data-x="${startPos.x}"][data-y="${+startPos.y + i}"]`
-        ).classList = classAdd;
+        );
+        if (!e.classList.value.includes('pship')) e.classList = classAdd;
       }
     }
   }
-
-  function shipPlacement(size, element, isHorizontal) {
+  function doShipColide(size, element, isHorizontal = true) {
+    const startPos = {
+      x: +element.getAttribute(`data-x`) - 1,
+      y: +element.getAttribute(`data-y`) - 1,
+    };
+    if (isHorizontal) {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < size + 2; j++) {
+          const e = document.querySelector(
+            `.player div[data-x="${j + startPos.x}"]` +
+              `[data-y="${i + startPos.y}"]`
+          );
+          if (e && e.classList.value.includes('pship')) return true;
+        }
+      }
+    } else {
+      for (let i = 0; i < size + 2; i++) {
+        for (let j = 0; j < 3; j++) {
+          const e = document.querySelector(
+            `.player div[data-x="${j + startPos.x}"]` +
+              `[data-y="${i + startPos.y}"]`
+          );
+          if (e && e.classList.value.includes('pship')) return true;
+        }
+      }
+    }
+    return false;
+  }
+  function shipHover(size, element, isHorizontal) {
     const x = checkSize(size, element, isHorizontal);
-    addHover(x.renderSize, element, x.check, isHorizontal);
+    const isValid = !doShipColide(size, element, isHorizontal) && x.check;
+    addClasses(x.renderSize, element, isValid, isHorizontal);
     element.addEventListener('mouseout', removeHover);
   }
+  const shlength = 3;
+  const shisHorizontal = false;
   document.querySelector('.player').addEventListener('mouseover', (e) => {
-    const length = 3;
-    const isHorizontal = false;
     // safety check
-    if (!e.target.classList.value) {
-      shipPlacement(length, e.target, isHorizontal);
-
+    if (!e.target.classList.value.includes('gameboard')) {
+      shipHover(shlength, e.target, shisHorizontal);
       // console.log({
       //   x: e.target.getAttribute('data-x'),
       //   y: e.target.getAttribute('data-y'),
       // });
     }
+  });
+  document.querySelector('.player').addEventListener('click', (e) => {
+    if (!e.target.classList.value.includes('gameboard'))
+      if (
+        !doShipColide(shlength, e.target, shisHorizontal) &&
+        checkSize(shlength, e.target, shisHorizontal).check
+      )
+        addClasses(shlength, e.target, true, shisHorizontal, true);
   });
 }
 
