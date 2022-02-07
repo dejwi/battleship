b83bee58202602c1ad5428b45c0ab7deef3cf378
dejwi <misti.krasula@gmail.com>
2022-02-07 03:41:09 +0100

    not needed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './styles/main.sass';
-import { drawEnemyShipsDebug } from './scripts/debug';
 import { gameplayHandler } from './scripts/gameplay';
 
-drawEnemyShipsDebug();
 gameplayHandler();
